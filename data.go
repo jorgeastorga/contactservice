848b6194d67107ac22420ee4927cba26b1676dd2
Jorge E. Astorga <jeastorga@gmail.com>
2017-02-17 23:08:36 -0800

    changed the service to use gorm

--- data.go
+++ data.go
@@ -1,80 +1,15 @@
 package main
 
 import (
-	"database/sql"
 	_"github.com/lib/pq"
 	"log"
-	_"github.com/jinzhu/gorm"
-        _"github.com/go-sql-driver/mysql"
-
-	"fmt"
+	"github.com/jinzhu/gorm"
 )
-
-
-//DB Connection Details
-const(
-	DBHost = "127.0.0.1"
-	DBPort = "3306"
-	DBUser = "root"
-	DBPass = "testing123"
-	DBDbase = "contakx"
-)
-
-//Database package-level identifier
-var Db *sql.DB
-
-//Database identifier based on gorm.DB
-//var database gorm.Db
-
-func init(){
-	log.Println("init called")
-	var err error
-
-	/** Setup Database Settings ***/
-	Db, err = sql.Open("postgres", "user=gwp dbname=gwp password=gwp ssl-mode=disable")
-	if err != nil {
-		panic(err)
-	}
-
-	/** Test Database Connection **/
-	err = Db.Ping()
-	if err != nil {
-		log.Println(err)
-		//panic(err)
-	}
-
-
-	dbConnection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
-		DBUser,
-		DBPass,
-		DBHost,
-		DBPort,
-		DBDbase)
-
-	var mySqlDB *sql.DB
-	mySqlDB, error := sql.Open("mysql", dbConnection)
-	if error != nil {
-		log.Println("Could not connect to the database: ", error)
-		log.Println(error.Error())
-	}else {
-		log.Println("DB Connection: connected to the database successfully")
-	}
-
-	err = mySqlDB.Ping()
-	if err != nil {
-		log.Println("Could not ping the database")
-		log.Println(err.Error())
-	}else{
-		log.Println("DB Ping was successful!")
-	}
-
-
-
-}
 
 
 type Contact struct {
-	Id int `json:"id"`
+	gorm.Model // declaring the model (based on Gorm)
+	//Id int `json:"id"`
 	FirstName string `json:"firstName"`
 	LastName string `json:"lastName"`
 	Address1 string `json:"address1"`
@@ -87,17 +22,21 @@ type Contact struct {
 	Phone string `json:"phone"`
 }
 
+type Product struct{
+	gorm.Model
+	Code string
+	Price float64
+}
 /**
  * Function used to create  a contact in the db
  */
 func (c *Contact)create() (err error){
-	statement := "insert into contacts (firstname, lastname, address1) values ($1, $2, $3) returning id"
-	stmt, err := Db.Prepare(statement)
-	if err != nil {
-		return
+
+	err = AppDB.Create(&Contact{FirstName:c.FirstName, LastName: c.LastName}).Error
+	if err != nil{
+		log.Println("Error Creating the Contact: ", err.Error())
 	}
-	defer stmt.Close()
-	err = stmt.QueryRow(c.FirstName, c.LastName, c.Address1).Scan(&c.Id)
+
 	return err
 }
 
@@ -106,9 +45,9 @@ func (c *Contact)create() (err error){
 *
 */
 func retrieve(id int) (contact Contact, err error) {
-	contact = Contact{}
+	/*contact = Contact{}
 	err = Db.QueryRow("select id, firstName, lastName, address1 from contacts where id = $1",
-		id).Scan(&contact.Id, &contact.FirstName, &contact.LastName, &contact.Address1)
+		id).Scan(&contact.Id, &contact.FirstName, &contact.LastName, &contact.Address1)*/
 	return contact, err
 
 }
@@ -120,8 +59,8 @@ func retrieve(id int) (contact Contact, err error) {
 *
 */
 func (c *Contact) update() (err error){
-	_, err = Db. Exec("update contacts set firstname = $2, lastname = $3, address1 = $3 where id = $1",
-		c.Id, c.FirstName, c.LastName, c.Address1)
+	/*_, err = Db. Exec("update contacts set firstname = $2, lastname = $3, address1 = $3 where id = $1",
+		c.Id, c.FirstName, c.LastName, c.Address1)*/
 	return err
 }
 
@@ -130,7 +69,7 @@ func (c *Contact) update() (err error){
 *
  */
 func (c *Contact)delete() (err error){
-	_, err = Db. Exec("delete from contacts where id = $1", c.Id)
+	//_, err = Db. Exec("delete from contacts where id = $1", c.Id)
 	return
 }
 
--- server.go
+++ server.go
@@ -6,17 +6,70 @@ import(
 	"path"
 	"strconv"
 	"log"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_"github.com/go-sql-driver/mysql" //database driver
 )
 
+//DB Connection Details
+const(
+	DBHost = "127.0.0.1"
+	DBPort = "3306"
+	DBUser = "root"
+	DBPass = "testing123"
+	DBDbase = "contakx"
+)
+
+//Database identifier based on gorm.DB
+var AppDB *gorm.DB
+
+func init(){
+	var err error
+
+	dbConnection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		DBUser,
+		DBPass,
+		DBHost,
+		DBPort,
+		DBDbase)
+
+	AppDB, err = gorm.Open("mysql", dbConnection)
+
+	if err != nil {
+		log.Println("Failed to connect to database: ", err.Error())
+	}else {
+		log.Println("DB Connection: connected to the database successfully")
+	}
+
+	AppDB.Create(&Product{Code:"L1212", Price: 1000})
+	AppDB.Create(&Contact{FirstName:"Jorge", LastName:"Astorga"})
+
+	//defer AppDB.Close()
+
+	//Migrate Schema
+	AppDB.AutoMigrate(&Contact{}, &Product{})
+
+	//Testing the creation of a contact
+	newContact := &Contact{FirstName:"Jorge", LastName:"Astorga"}
+	newContact.create()
+}
 
 func main(){
+
+	//Testing to add a contact
+	AppDB.Create(&Contact{FirstName:"Oralge", LastName:"Orale"})
+
+
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 
 	http.HandleFunc("/contact/", handleRequest)
 	server.ListenAndServe()
+
 }
+
+
 
 func handleRequest(w http.ResponseWriter, r *http.Request){
 	var err error
@@ -37,6 +90,10 @@ func handleRequest(w http.ResponseWriter, r *http.Request){
 	}
 }
 
+/*
+ *
+ *
+ */
 func handleGet(w http.ResponseWriter, r *http.Request) (err error){
 	id, err := strconv.Atoi(path.Base(r.URL.Path))
 	if err != nil {
