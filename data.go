0e6a2656909862181031e57f659238b657bfe7f6
Jorge E. Astorga <jeastorga@gmail.com>
2017-02-17 19:01:00 -0800

    added code to establish connection to a mysql database

--- data.go
+++ data.go
@@ -4,23 +4,71 @@ import (
 	"database/sql"
 	_"github.com/lib/pq"
 	"log"
+	_"github.com/jinzhu/gorm"
+        _"github.com/go-sql-driver/mysql"
+
+	"fmt"
 )
 
+
+//DB Connection Details
+const(
+	DBHost = "127.0.0.1"
+	DBPort = "3306"
+	DBUser = "root"
+	DBPass = "testing123"
+	DBDbase = "contakx"
+)
+
+//Database package-level identifier
 var Db *sql.DB
+
+//Database identifier based on gorm.DB
+//var database gorm.Db
 
 func init(){
 	log.Println("init called")
 	var err error
+
+	/** Setup Database Settings ***/
 	Db, err = sql.Open("postgres", "user=gwp dbname=gwp password=gwp ssl-mode=disable")
 	if err != nil {
 		panic(err)
 	}
 
+	/** Test Database Connection **/
 	err = Db.Ping()
 	if err != nil {
 		log.Println(err)
-		panic(err)
+		//panic(err)
 	}
+
+
+	dbConnection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		DBUser,
+		DBPass,
+		DBHost,
+		DBPort,
+		DBDbase)
+
+	var mySqlDB *sql.DB
+	mySqlDB, error := sql.Open("mysql", dbConnection)
+	if error != nil {
+		log.Println("Could not connect to the database: ", error)
+		log.Println(error.Error())
+	}else {
+		log.Println("DB Connection: connected to the database successfully")
+	}
+
+	err = mySqlDB.Ping()
+	if err != nil {
+		log.Println("Could not ping the database")
+		log.Println(err.Error())
+	}else{
+		log.Println("DB Ping was successful!")
+	}
+
+
 
 }
 
